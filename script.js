1714672774e576ad13f93ac14e6c170decb5cc84
mrlaboratory <admin@mrlaboratory.com>
2020-10-15 13:08:10 +0600

    add html css js file for this

---
+++ script.js
@@ -0,0 +1,8 @@
+// $(".imgResMR").mouseover(function(){
+//     $(".imgResMR .buttonDiv").slideDown();
+//   });
+ 
+//   $(".imagesSliderMR img, .imgResMR .buttonDiv").mouseout(function(){
+//     $(".imgResMR .buttonDiv").slideUp();
+//   });
+

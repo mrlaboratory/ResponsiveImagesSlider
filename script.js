51b0c34c1977e6aa1c883a6d3c6221e46acf2705
mrlaboratory <admin@mrlaboratory.com>
2020-10-17 23:35:48 +0600

    add script js file for this project

--- script.js
+++ script.js
@@ -6,3 +6,32 @@
 //     $(".imgResMR .buttonDiv").slideUp();
 //   });
 
+var photos = ['img/1.png','img/2.png','img/3.png','img/4.png','img/5.png'];
+
+var img = document.querySelector('.imagesSliderMR img');
+
+var i = 0;
+function next(){
+if(i<photos.length-1){
+   i++;
+   img.src=photos[i];
+
+}else{
+
+   i=0;
+   img.src=photos[i];
+}
+
+}
+function prev(){
+   if(i>0){
+      i--;
+      img.src=photos[i];
+   
+   }else{
+   
+      i=photos.length-1;
+      img.src=photos[i];
+   }
+   
+   }
